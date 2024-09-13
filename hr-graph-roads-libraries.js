39f845a92d3fbe5f8100ddf1a0a905f96d17abd5
Leah De Laurell <52628574+leahdelaurell@users.noreply.github.com>
2024-09-12 20:59:08 -0600

    added new roads and library

---
+++ hr-graph-roads-libraries.js
@@ -0,0 +1,70 @@
+/**
+ * https://www.hackerrank.com/challenges/torque-and-development/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=graphs
+ * 
+ * 
+ * This is not correct and I need to look into it more.
+ * 
+ * @param n number of cities
+ * @param c_lib cost of library
+ * @param c_road cost of road
+ * @param cities array to cities
+ * @returns 
+ */
+
+/*
+ * Complete the 'roadsAndLibraries' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n number of cities
+ *  2. INTEGER c_lib cost of library
+ *  3. INTEGER c_road cost of road
+ *  4. 2D_INTEGER_ARRAY cities array to cities
+ */
+
+function roadsAndLibraries(n, c_lib, c_road, cities) {
+    const routes = {}
+    for (let _cities of cities) {
+        const [c1, c2] = _cities
+        if (routes[c1]) {
+            routes[c1].cities.push(c2)
+        } else {
+            routes[c1] = {
+                cities: [c2],
+                visited: false
+            }
+        }
+        
+        if (routes[c2]) {
+            routes[c2].cities.push(c1)
+        } else {
+            routes[c2] = {
+                cities: [c1],
+                visited: false
+            }
+        }
+    }
+    console.log(routes)
+    let roads = 0
+    let library = 0
+    for(let route of Object.values(routes)) {
+        console.log(route)
+        if (route.visited) continue
+        roads += dfs(routes, route, 0)
+        // need to add a library if hit all the routes
+        library++
+    }
+    
+    // need to understand this better?? Why adding the ?
+    library += n - Object.keys(routes).length
+    return (library*c_lib) + (roads*c_road)
+}
+
+function dfs(routes, route, roads) {
+    if (route.visited) return roads
+    route.visited = true
+    for (let cities of route.cities) {
+        roads += dfs(routes, routes[cities], roads)
+    }
+    return roads + 1
+}
