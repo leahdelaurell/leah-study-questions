df76765781a483828444ccdb86911b69f4c9e90f
Leah De Laurell <52628574+leahdelaurell@users.noreply.github.com>
2024-09-18 15:36:57 -0600

    updated my coin change

--- lc-coin-change.js
+++ lc-coin-change.js
@@ -2,6 +2,8 @@
  * https://leetcode.com/problems/coin-change/?envType=study-plan-v2&envId=top-interview-150
  * 
  * 
+ * Ended up watching the coding video for this and it made more sense doing it with dynamic programming
+ * 
  * This is inefficent but it works for most use cases! Going to see if I can
  * make it more efficent before I look at the discussion
  * 
@@ -9,53 +11,26 @@
  * @param {number} amount
  * @return {number}
  */
+
 var coinChange = function(coins, amount) {
-  if (amount == 0) {
-      return 0
-  }
-  let minLength 
-  for (let i=0; i<coins.length; i++) {
-      let combo = 0
-      const _currentAmount = amount - coins[i]
-      // if current amount hasn't been filled continue to find combo
-      if (_currentAmount > 0) {
-          combo++
-          combo = findCombo(coins, _currentAmount, combo, minLength)
-      } else if (_currentAmount === 0) {
-          combo++
-      } else {
-          continue
-      }
+    const amountarray = new Array(amount+1).fill(amount+1)
+    amountarray[0] = 0
+    // console.log(amountarray)
+    for (let i = 1; i < amountarray.length; i++) {
+        for(let j = 0; j < coins.length; j++) {
+            const _sub = i - coins[j]
+            // check if the subtraction of the coin is still a possible value
+            if (_sub >= 0) {
+                // if so we can grab the minimum value of those coins added up
+                // and add one on top of it for the one we just subtracted
+                const _addingMinCoins = amountarray[_sub] + 1
+                // then we can check if that coin is the minimum amount 
+                if (_addingMinCoins < amountarray[i]) {
+                    amountarray[i] = _addingMinCoins
+                }
+            }
+        }
+    }
 
-      if (minLength && combo > 0 && minLength > combo) {
-          minLength = combo
-      } else if (!minLength && combo > 0) {
-          minLength = combo
-      }
-  }
-  return minLength ? minLength : -1
+    return amountarray[amount] !== amount+1 ? amountarray[amount] : -1
 };
-
-function findCombo(coins, amount, combo, minLength) {
-  for (let i = 0; i<coins.length; i++) {
-      let _newCombo = combo
-      let _currAmount = amount - coins[i]
-      if (_currAmount > 0) {
-          _newCombo++
-          _newCombo = findCombo(coins, _currAmount, _newCombo, minLength)
-      } else if (_currAmount === 0) {
-          _newCombo++
-      } else if (_currAmount < 0) {
-          continue
-      }
-
-      if (minLength && minLength > _newCombo) {
-          minLength = _newCombo
-      } else if (!minLength && _newCombo > 0) {
-          minLength = _newCombo
-      }
-      _newCombo--
-  }
-
-  return minLength
-}
